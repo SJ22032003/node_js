55a5b897c7109a6d3d5e62ac3b52b1ca15324e23
Shobhit Jaryal <jaryalshaurya2203@gmail.com>
2022-08-28 15:08:30 +0530

    core modules and fs

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log("Node");
+
+const fs = require('fs') // file system module
+
+// Creating a new File
+fs.writeFileSync('./explain/notes.txt', 'This is my first file')
+
+// Data Overwrite if File is Present
+fs.writeFileSync('./explain/notes.txt', 'Data Overwrite if File is Present ')
+
+// Appending Data to File
+fs.appendFileSync('./explain/notes.txt', 'Appending Data to File ')
+fs.appendFileSync('./explain/notes.txt', 'Testing node.js');
+
+//Node. js includes an additional data type called Buffer
+//(not available in browser's JavaScript).
+// while reading from a file or receiving packets over the network.
+//Buffer is mainly used to store binary data,
+
+const buf_data = fs.readFileSync("./explain/notes.txt");
+const org_data = buf_data.toString();
+console.log(org_data);
+
+// Rename File
+fs.renameSync('./explain/testNotes.txt', './explain/notes.txt');
